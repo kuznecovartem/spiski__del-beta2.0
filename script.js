39643d60e732c3e9d88f7ddc7b7b262c4fb89e18
kuznecovartem <122590171+kuznecovartem@users.noreply.github.com>
2024-01-29 16:43:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const myButton = document.querySelector('#myButton');
+const nameInput = document.querySelector('#nameInput');
+const list = document.querySelector('#list');
+
+
+myButton.addEventListener('click', (event) => {
+    const newItem = document.querySelector('li');
+    newItem.classList.add('Item');
+    newItem.textContent = nameInput.value;
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'удалить';
+    deleteButton.addEventListener('click', () => {
+        list.removeChild(newItem);
+    })
+    newItem.appendChild('deleteButton');
+    list.appendChild(newItem);
+
+    nameInput.value = '';
+})
